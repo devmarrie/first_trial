7c22960c658a2f508fd6ee4917ce5ee2d0932d23
joethesaint <joebamisaye068@gmail.com>
2022-07-12 22:15:16 -0500

    [style]: remove spaces at start of line

--- print_char.c
+++ print_char.c
@@ -9,10 +9,11 @@
 
 int print_char(char *buff_dest, va_list arg, int buff_count)
 {
-       char c = var_arg(arg, int);
+	char c = va_arg(arg, int);
 
-       buff_dest[buff_count] = c
+	buff_dest[buff_count] = c;
 
-       return (++buff_count);
+	return (++buff_count);
 
 }
+
